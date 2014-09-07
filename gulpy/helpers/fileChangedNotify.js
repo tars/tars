8f826ef29fe6c39b18de8a2448fe5a9cccb0db70
Artem Malko <a.malko@2gis.ru>
2014-09-07 13:45:22 +0700

    remove coments

--- gulpy/helpers/fileChangedNotify.js
+++ gulpy/helpers/fileChangedNotify.js
@@ -3,5 +3,4 @@ var gutil = require('gulp-util');               // Gulp util module
 // Output name of modified file to console
 module.exports = function(filename) {
     gutil.log('file was changed: ', gutil.colors.cyan(filename));
-    // gutil.beep();
 };
