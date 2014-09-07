21dc758feb1230263de037823ed0f85321d1cea2
Artem Malko <a.malko@2gis.ru>
2014-09-07 14:34:11 +0700

    remove templater

--- gulpy/tasks/init.js
+++ gulpy/tasks/init.js
@@ -7,22 +7,46 @@ var gulp = require('gulp'),                                     // Gulp JS
     notifyConfig = projectConfig.notifyConfig,                  // Notify config
     modifyDate = require('../helpers/modifyDateFormatter'),     // Date formatter for notify
     ghdownload = require('github-download');
-    ncp = require('ncp').ncp;
+    ncp = require('ncp').ncp,
+    templateExtension = '',
+    projectConfigTemlater = projectConfig.templater.toLowerCase();
 
 require('./createFs')();
+
+var githubConfig = {
+    user: 'artem-malko',
+    repo: 'markupBuilder-extensions',
+    ref: 'master'
+};
+
+if (projectConfigTemlater === 'jade') {
+    templateExtension = 'jade';
+} else if (projectConfigTemlater === 'hadlebars' 
+        || projectConfigTemlater === 'hadelbars' 
+        || projectConfigTemlater === 'hdb' 
+        || projectConfigTemlater === 'hb') {
+    templateExtension = 'hadlebars';
+} else {
+    templateExtension = 'jade';
+}
 
 // Task description
 module.exports = function() {
     
     return gulp.task('init', ['create-fs'], function() {
-        // ghdownload({user: 'artem-malko', repo: 'test-github', ref: 'master'}, process.cwd() + '/temp')
-        // .on('end', function() {
-        //     ncp(process.cwd() + '/temp', process.cwd(), function (err) {
 
-        //             if (err) {
-        //                 console.log('error');
-        //             }
-        //         });
+        // Including templater
+        ghdownload({user: githubConfig.user, repo: githubConfig.repo, ref: 'mkExt-' + templateExtension + '-templater'}, process.cwd() + '/.tmp-templater')
+        .on('end', function() {
+            ncp(process.cwd() + '/.tmp-templater', process.cwd(), function (err) {
+
+                    if (err) {
+                        console.log('error');
+                    }
+                });
+        });
+
+        // return gulp.src('./.tmp', { read: false })
+        //     .pipe(rimraf({ force: true }));
         // });
-    });
 };   
