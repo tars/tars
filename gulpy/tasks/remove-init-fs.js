2a1cc35a0ef0c815d86d3f930120dce62583487c
Artem Malko <a.malko@2gis.ru>
2014-11-11 17:02:26 +0600

    fi xjscs-config

--- gulpy/tasks/remove-init-fs.js
+++ gulpy/tasks/remove-init-fs.js
@@ -8,11 +8,12 @@ var gulp = require('gulp'),
     modifyDate = require('../helpers/modifyDateFormatter'),
 
     pathToDel = [
-                 'markup/' + projectConfig.fs.staticFolderName + '/js/libs', 
+                 'markup/' + projectConfig.fs.staticFolderName + '/js/libs',
                  'markup/' + projectConfig.fs.staticFolderName + '/js/plugins',
                  'markup/' + projectConfig.fs.staticFolderName + '/' + projectConfig.fs.imagesFolderName + '/',
                  'markup/' + projectConfig.fs.staticFolderName + '/fonts/',
                  'markup/' + projectConfig.fs.staticFolderName + '/scss/',
+                 'markup/' + projectConfig.fs.staticFolderName + '/stylus/',
                  'markup/' + projectConfig.fs.staticFolderName + '/less/',
                  'markup/modules/_template/assets/',
                  'markup/modules/_template/ie/',
@@ -20,12 +21,11 @@ var gulp = require('gulp'),
                  './markup/modules/footer/',
                  './markup/modules/_template/_template.scss',
                  './markup/modules/_template/_template.less',
+                 './markup/modules/_template/_template.styl',
                  './markup/modules/_template/_template.html',
-                 './markup/modules/_template/_template.jade', 
-                 './markup/pages/', 
-                 './markup/' + projectConfig.fs.staticFolderName + '/scss/',
-                 './markup/' + projectConfig.fs.staticFolderName + '/less/', 
-                 './.tmpTemplater/', 
+                 './markup/modules/_template/_template.jade',
+                 './markup/pages/',
+                 './.tmpTemplater/',
                  './.tmpPreproc/'
                 ];
 
@@ -34,4 +34,4 @@ module.exports = function() {
     return gulp.task('remove-init-fs', function(cb) {
         del(pathToDel, cb);
     });
-};   
+};
