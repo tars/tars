92a1d5c764feb64c78b765fe0c53cbba10a6781b
Bogdan Chadkin <trysound@yandex.ru>
2015-04-01 21:43:21 +0300

    Added throwing errors when process config

--- tars/helpers/process-config.js
+++ tars/helpers/process-config.js
@@ -1,6 +1,8 @@
 var fs = require('fs'),
+    gutil = require('gulp-util'),
     config = require('../../tars-config.js');
 
+// Registry of modules
 var tarsModules = {
     'jade': 'https://github.com/artem-malko/tars-jade',
     'handlebars': 'https://github.com/artem-malko/tars-handlebars',
@@ -10,11 +12,34 @@ var tarsModules = {
 };
 
 module.exports = function () {
-    config.templater = typeof config.templater === 'string' && config.templater.toLowerCase();
-    config.processor = typeof config.cssPreprocessor === 'string' && config.cssPreprocessor.toLowerCase();
+    // Names
+    var templater = typeof config.templater === 'string' && config.templater.toLowerCase();
+    var processor = typeof config.cssPreprocessor === 'string' && config.cssPreprocessor.toLowerCase();
 
-    config.templaterRepo = tarsModules[config.templater] || false;
-    config.processorRepo = tarsModules[config.processor] || false;
+    // Check modules valid
+    if(typeof tarsModules[templater] !== 'string') {
+        throw new gutil.PluginError('TARS', [
+                gutil.colors.blue('"' + config.templater + '"'),
+                gutil.colors.red(' is invalid templater, please check '),
+                gutil.colors.magenta('tars-config.js')
+            ].join(''));
+    }
+
+    if(typeof tarsModules[processor] !== 'string') {
+        throw new gutil.PluginError('TARS', [
+                gutil.colors.blue('"' + config.cssPreprocessor + '"'),
+                gutil.colors.red(' is invalid processor, please check '),
+                gutil.colors.magenta('tars-config.js')
+            ].join(''));
+    }
+
+    // Apply names to config
+    config.templater = templater;
+    config.processor = processor;
+
+    // Repositories
+    config.templaterRepo = tarsModules[templater];
+    config.processorRepo = tarsModules[processor];
 
     // Set template's extension
     switch(config.templater) {
