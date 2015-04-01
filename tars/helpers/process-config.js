a9e17917f8e37ca374b6ad83218f65179d9fe785
Bogdan Chadkin <trysound@yandex.ru>
2015-04-01 17:03:14 +0300

    Added process-config helper

---
+++ tars/helpers/process-config.js
@@ -0,0 +1,49 @@
+var fs = require('fs'),
+    config = require('../../tars-config.js');
+
+var tarsModules = {
+    'jade': 'https://github.com/artem-malko/tars-jade',
+    'handlebars': 'https://github.com/artem-malko/tars-handlebars',
+    'stylus': 'https://github.com/artem-malko/tars-stylus',
+    'less': 'https://github.com/artem-malko/tars-less',
+    'scss': 'https://github.com/artem-malko/tars-scss'
+};
+
+module.exports = function () {
+    config.templater = typeof config.templater === 'string' && config.templater.toLowerCase();
+    config.processor = typeof config.cssPreprocessor === 'string' && config.cssPreprocessor.toLowerCase();
+
+    config.templaterRepo = tarsModules[config.templater] || false;
+    config.processorRepo = tarsModules[config.processor] || false;
+
+    // Set template's extension
+    switch(config.templater) {
+        case 'handlebars':
+            config.templateExtension = 'hbs';
+            break;
+        case 'jade':
+            config.templateExtension = 'jade';
+            break;
+        default:
+            config.templateExtension = 'html';
+            break;
+    }
+
+    // Set processor's extension
+    switch(config.processor) {
+        case 'stylus':
+            config.styleExtention = 'styl';
+            break;
+        case 'scss':
+            config.styleExtention = 'scss';
+            break;
+        case 'less':
+            config.styleExtention = 'less';
+            break;
+        default:
+            config.styleExtention = 'css';
+            break;
+    }
+
+    return config;
+};
