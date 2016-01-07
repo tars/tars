85ba2709d3a07c245cfa4bace929eaa226a8fc56
Artem Malko <artem.malko@gmail.com>
2016-01-08 00:59:45 +0600

    update dependencies

--- tars/tasks/css/make-sprite-for-svg.js
+++ tars/tasks/css/make-sprite-for-svg.js
@@ -5,21 +5,21 @@ const plumber = tars.packages.plumber;
 const notifier = tars.helpers.notifier;
 const skipTaskWithEmptyPipe = tars.helpers.skipTaskWithEmptyPipe;
 
-const staticFolderName = tars.config.fs.staticFolderName;
-const imagesFolderName = tars.config.fs.imagesFolderName;
-const preprocExtension = tars.cssPreproc.mainExt;
 const preprocName = tars.cssPreproc.name;
+const preprocExtension = tars.cssPreproc.mainExt;
+const imagesFolderPath = './dev/' + tars.config.fs.staticFolderName + '/' + tars.config.fs.imagesFolderName;
+const preprocFoldePath = './markup/' + tars.config.fs.staticFolderName + '/' + preprocName;
+
 
 /**
  * Make sprite for svg and styles for this sprite
  * Return pipe with styles for sprite
  */
 module.exports = () => {
-
     return gulp.task('css:make-sprite-for-svg', (cb) => {
 
         if (tars.config.useSVG) {
-            return gulp.src('./dev/' + staticFolderName + '/' + imagesFolderName + '/minified-svg/*.svg')
+            return gulp.src(imagesFolderPath + '/minified-svg/*.svg')
                 .pipe(plumber({
                     errorHandler: (error) => {
                         notifier.error('An error occurred while making sprite for svg.', error);
@@ -28,14 +28,12 @@ module.exports = () => {
                 .pipe(skipTaskWithEmptyPipe('css:make-sprite-for-svg', cb))
                 .pipe(tars.require('gulp-svg-spritesheet')({
                     cssPathSvg: '',
-                    templateSrc: './markup/' + staticFolderName
-                                    + '/' + preprocName + '/sprite-generator-templates/'
+                    templateSrc: preprocFoldePath + '/sprite-generator-templates/'
                                     + preprocName + '.svg-sprite.mustache',
-                    templateDest: './markup/' + staticFolderName
-                                    + '/' + preprocName + '/sprites-' + preprocName
+                    templateDest: preprocFoldePath + '/sprites-' + preprocName
                                     + '/svg-sprite.' + preprocExtension
                 }))
-                .pipe(gulp.dest('./dev/' + staticFolderName + '/' + imagesFolderName + '/svg-sprite/sprite.svg'))
+                .pipe(gulp.dest(imagesFolderPath + '/svg-sprite/sprite.svg'))
                 .pipe(
                     notifier.success(
                         preprocName.charAt(0).toUpperCase() + preprocName.slice(1) + ' for svg-sprite is ready.'
